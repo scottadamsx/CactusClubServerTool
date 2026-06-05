d1a9da92f19a3f3e6255474ccb699eb92f5fc775
Scott Adams <scottadamsx@gmail.com>
2026-06-05 11:37:48 -0230

    Added Header Component

---
+++ cactusclub-servertool/app/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+          <h1>Cactus Club Cafe</h1>
+          <h3>Server Tool</h3>
+        </header>
+  )
+}
+
+export default Header
